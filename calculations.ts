282877a2794a1e3da44f6828e117922d72e2deb2
anastasiya-harbatsevich <nastya7535@gmail.com>
2025-07-21 16:30:18 +0200

    Move calculation function, improve error, made changes in readme and package files

--- calculations.ts
+++ calculations.ts
@@ -1,47 +1,45 @@
-import promptSync from 'prompt-sync';
-import {add, subtract, multiply, divide} from './operations';
+import promptSync from "prompt-sync";
+import { add, subtract, multiply, divide } from "./operations";
 
 const prompt = promptSync();
 
-const firstInput = prompt('Enter the first number: ');
-const secondInput = prompt('Enter the second number: ');
-let operatorInput = prompt('Enter operator: ');
+const firstInput = prompt("Enter the first number: ");
+const secondInput = prompt("Enter the second number: ");
+let operatorInput = prompt("Enter operator: ");
 
 const a = parseFloat(firstInput);
 const b = parseFloat(secondInput);
-
-if (isNaN(a) || isNaN(b)) {
-    console.log('Entered symbol isnt a number');
-    process.exit(1);
-};
-
-const operators = ['+', '-', '*', '/'];
+const operators = ["+", "-", "*", "/"];
 
 while (!operators.includes(operatorInput)) {
-    console.log('Unknown operator');
-    operatorInput = prompt('Enter correct operator: ')
+  console.log("Unknown operator");
+  operatorInput = prompt("Enter correct operator: ");
 }
 
-try {
+if (!isNaN(a) && !isNaN(b)) {
+  try {
     let result: number;
     switch (operatorInput) {
-        case '+' :
-            result = add(a, b);
-            break;
-        case '-' :
-            result = subtract(a, b);
-            break;
-        case '*' :
-            result = multiply(a, b);
-            break;
-        case '/' :
-            result = divide(a, b);
-            break;
-        default :
-            console.log('Unknown operation');
-            process.exit(1);
+      case "+":
+        result = add(a, b);
+        break;
+      case "-":
+        result = subtract(a, b);
+        break;
+      case "*":
+        result = multiply(a, b);
+        break;
+      case "/":
+        result = divide(a, b);
+        break;
+      default:
+        throw new Error("Unknown operator");
     }
     console.log(`Result: ${a} ${operatorInput} ${b} = ${result}`);
-} catch (error) {
+  } catch (error) {
     console.log(`${(error as Error).message}`);
+  }
+} else {
+  console.log("Entered symbol isnt a number");
+  process.exit(1);
 }
